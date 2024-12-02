1758cd2f309dc52af2291f207d95950fa4ef2492
Mathieu <beuret_m@etna-alternance.net>
2024-12-02 16:26:07 +0100

    Initial Theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
+import { ThemeProvider } from "@emotion/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
-import "./index.css";
+import "./main.css";
+import theme from "./theme.ts";
 import "./assets/locale/i18n.ts";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<Router>
-			<App />
-		</Router>
+		<ThemeProvider theme={theme}>
+			<Router>
+				<App />
+			</Router>
+		</ThemeProvider>
 	</StrictMode>
 );
