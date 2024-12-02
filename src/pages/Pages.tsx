d3411c19107e65451cb041180e04484b4afa6307
Mathieu <beuret_m@etna-alternance.net>
2024-12-02 16:14:38 +0100

    Translation + Router & Eslint fixs

--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -4,24 +4,12 @@ import HomePage from "./Home";
 import Layout from "./Layout";
 import LegalPage from "./Legal";
 import LoginPage from "./Login";
-import {
-	Admin,
-	Home,
-	Legal,
-	Login,
-	Photos,
-	Profile,
-	Register,
-	Test,
-	Videos,
-} from "./paths";
+import { Home, Legal, Login, Photos, Register, Test, Videos } from "./paths";
 import PhotoPage from "./Photos";
 import RegisterPage from "./Register";
 import TestPage from "./Test";
 import VideosPage from "./Videos";
 import ErrorComponent from "../components/error";
-import AdminPage from "./Admin";
-import ProfilePage from "./Profile/Profile";
 
 const Pages = () => {
 	// const { hasMinRole } = useRole();
@@ -40,21 +28,18 @@ const Pages = () => {
 			<Route path={Login} element={<LoginPage />} />
 			<Route path={Register} element={<RegisterPage />} />
 
-			<Route path={Admin} element={<AdminPage />} />
-			<Route path={Profile} element={<ProfilePage />} />
-
 			{/* <Route element={<ProtectedRoute minRole={UserRoles.PendingUser} />}>
-                <Route path={Home} element={<Layout />}>
-                    {hasMinRole(UserRoles.Moderator) ? (
-                        <>
-                            <Route path={Admin} element={<AdminPage />} />
+				<Route path={Home} element={<Layout />}>
+					{hasMinRole(UserRoles.Moderator) ? (
+						<>
+							<Route path={Admin} element={<AdminPage />} />
 							<Route path={Profile} element={<ProfilePage />} />
-                        </>
-                    ) : (
-                        <Route path={Waiting} element="TestPage" />
-                    )}
-                </Route>
-            </Route> */}
+						</>
+					) : (
+						<Route path={Waiting} element="TestPage" />
+					)}
+				</Route>
+			</Route> */}
 		</Routes>
 	);
 };
