94ee39b39e733edaa58e3cc0bd2bfd6c1fb15d6c
Mathieu <beuret_m@etna-alternance.net>
2024-12-02 16:55:56 +0100

    theme manipulation
    add fonts
    start transfer css rules to material theme

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.tsx";
 import "./main.css";
 import theme from "./theme.ts";
+import { CssBaseline } from "@mui/material";
 import "./assets/locale/i18n.ts";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<ThemeProvider theme={theme}>
+			<CssBaseline />
 			<Router>
 				<App />
 			</Router>
--- src/theme.ts
+++ src/theme.ts
@@ -2,6 +2,14 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
 	palette: {
+		background: {
+			default: "radial-gradient(#021027, #000000)",
+		},
+		text: {
+			primary: "#FFFFFFDE",
+			secondary: "#FFFFFFDE",
+			disabled: "#FFFFFFDE",
+		},
 		primary: {
 			main: "#1976d2",
 		},
@@ -11,6 +19,38 @@ const theme = createTheme({
 	},
 	typography: {
 		fontFamily: "SansationLight, sans-serif",
+		h1: {
+			fontSize: "2.5rem",
+			lineHeight: 1.1,
+		},
+	},
+	components: {
+		MuiCssBaseline: {
+			styleOverrides: {
+				a: {
+					color: "#FFFFFFDE !important",
+					textDecoration: "none !important",
+				},
+				":root": {
+					lineHeight: 1.5,
+					fontWeight: 400,
+					colorScheme: "light dark",
+					fontSynthesis: "none",
+					textRendering: "optimizeLegibility",
+					WebkitFontSmoothing: "antialiased",
+					MozOsxFontSmoothing: "grayscale",
+					background: "radial-gradient(#021027, #000000)",
+				},
+				body: {
+					margin: 0,
+					display: "flex",
+					placeItems: "center",
+					minWidth: "400px",
+					minHeight: "100vh",
+					fontFamily: "'SansationLight', sans-serif !important",
+				},
+			},
+		},
 	},
 });
 
